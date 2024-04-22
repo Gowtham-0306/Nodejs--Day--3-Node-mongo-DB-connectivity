65fa8a0da4e9a8a99081a9e5aa9e369f72b6304b
Gowtham-0306 <141541254+Gowtham-0306@users.noreply.github.com>
2024-04-22 13:55:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ httpserver.use(bodyparser.json());
 
 
 // starts a simple http server locally on port 3000
-httpserver.listen(3000,  () => {
+httpserver.listen(process.env.PORT, process.env.HOSTNAME,   () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
